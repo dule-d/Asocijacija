874920b73cddf3913c94fcb2aa448e5bb61483a8
dule <duki.ilieski@gmail.com>
2025-05-14 21:10:33 +0200

    Icon
    Icon and art change.

--- Scripts/how_to_play.gd
+++ Scripts/how_to_play.gd
@@ -11,9 +11,5 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_red_button_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://Main Menues/play_menu.tscn")
-
-
-func _on_pink_button_h2p_pressed() -> void:
-	get_tree().change_scene_to_file("res://Main Menues/how_to_play.tscn")
+func _on_back_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Main Menues/mainMenu.tscn")
